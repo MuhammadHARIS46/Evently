411ab143306fd495cde0ffc15de7e21615d553f6
MuhammadHARIS46 <muhammadhariskuk123@gmail.com>
2023-12-28 22:21:46 +0500

    database models set up user creation updation and deletion events tied to clerk

--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+'use client';
+import React from "react";
+import { headerLinks } from "@/constants";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const NavItems = () => {
+  const pathname = usePathname();
+
   return (
-    <div>
-      items
-    </div>
+    <ul className="md:flex-between flex w-full flex-col items-start gap-5 md:flex-row">
+      {headerLinks.map((link, index) => {
+        const isActive = pathname === link.route;
+        return (
+          <li
+            key={index}
+            className={`${
+              isActive && "text-primary-500"
+            } flex-center p-medium-16 whitespace-nowrap`}
+          >
+            <Link href={link.route}>{link.label}</Link>
+          </li>
+        );
+      })}
+    </ul>
   );
-}
+};
 
 export default NavItems;
