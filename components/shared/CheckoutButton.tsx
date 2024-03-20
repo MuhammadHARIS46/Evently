74dfe944fa6c40cb8e6ab637353f497304dc5235
MuhammadHARIS46 <muhammadhariskuk123@gmail.com>
2024-03-20 19:04:54 +0500

    typo change get tickets to get ticket

--- components/shared/CheckoutButton.tsx
+++ components/shared/CheckoutButton.tsx
@@ -19,7 +19,7 @@ const CheckoutButton = ({ event }: { event: IEvent }) => {
           <SignedOut>
             <Button asChild className="button rounded-full" size="lg">
               <Link href="/sign-in">
-                Get Tickets
+                Get Ticket
               </Link>
             </Button>
           </SignedOut>
